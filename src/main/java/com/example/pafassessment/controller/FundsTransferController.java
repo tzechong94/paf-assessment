15dacf04b0f37ad0c4b5a3ef94e3bb4bc8cc0921
tc94 <tze.chong1994@gmail.com>
2023-03-03 12:20:12 +0800

    c1 to c4 done. c0 and c5 not done

--- src/main/java/com/example/pafassessment/controller/FundsTransferController.java
+++ src/main/java/com/example/pafassessment/controller/FundsTransferController.java
@@ -6,12 +6,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.pafassessment.models.Account;
 import com.example.pafassessment.models.Transaction;
 import com.example.pafassessment.service.AccountService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class FundsTransferController {
@@ -26,21 +29,59 @@ public class FundsTransferController {
         // model.addAttribute(null, form)
 
         List<Account> listOfAccounts = accountSvc.getAllAccounts();
-        List<String> listOfNameAndAccountId = new ArrayList<String>();
+        // List<String> listOfNameAndAccountId = new ArrayList<String>();
 
-        for (Account x : listOfAccounts){
-            listOfNameAndAccountId.add(x.getName() + " (" + x.getAccountId() +")");
-        }
-
-        System.out.println(listOfNameAndAccountId);
-        System.out.println(listOfAccounts);
+        // for (Account x : listOfAccounts){
+        //     listOfNameAndAccountId.add(x.getName() + " (" + x.getAccountId() +")");
+        // }
+        String error = "err";
+        // System.out.println(listOfNameAndAccountId);
+        // System.out.println(listOfAccounts);
         model.addAttribute("listOfAccounts", listOfAccounts);
+        model.addAttribute("error", error);
         model.addAttribute("transaction", new Transaction());
         return "transferform";
     }
 
-    // @PostMapping("/transfer")
-    // public String postForm(Model model) {
+    @PostMapping("/transfer")
+    public String postForm(@Valid Transaction transaction, BindingResult bindingResult,
+    Model model) {
 
-    // }
+        String fromAccount = transaction.getFromAccount();
+        String toAccount = transaction.getToAccount();
+
+        if (bindingResult.hasErrors()){
+            List<Account> listOfAccounts = accountSvc.getAllAccounts();
+            
+            // System.out.println(listOfAccounts);
+            model.addAttribute("listOfAccounts", listOfAccounts);
+            System.out.println("both accounts not the same, but binding errors");
+            return "transferform"; 
+        }
+
+        if (toAccount.equals(fromAccount)) {
+            List<Account> listOfAccounts = accountSvc.getAllAccounts();
+            model.addAttribute("listOfAccounts", listOfAccounts);
+            System.out.println("accounts same error");
+
+            String error = "Both accounts are the same. Transaction failed.";
+            model.addAttribute("error", error);
+            return "transferform";
+        } 
+
+
+
+
+        System.out.println(fromAccount + " from account");
+        System.out.println(toAccount + " to account");
+
+        // if (toAccount.equals(fromAccount)) {
+        //     String error = "Both accounts are the same. Transaction failed.";
+        //     model.addAttribute("error", error);
+        //     return "transferform";
+        // }
+
+        return "summary";
+
+    }
 }
--- src/main/java/com/example/pafassessment/models/Transaction.java
+++ src/main/java/com/example/pafassessment/models/Transaction.java
@@ -2,6 +2,8 @@ package com.example.pafassessment.models;
 
 import java.util.Date;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,9 +15,17 @@ public class Transaction {
     
     private String transactionId;
     private Date date;
+
+    @Size(min= 10, max=10, message="Account number not 10 characters")
     private String fromAccount;
+
+    @Size(min= 10, max=10, message="Account number not 10 characters")
     private String toAccount;
+
+    @Min(value = 0,  message = "The amount must be positive")
+    @Min(value = 10,  message = "The minimum transfer is $10.")
     private Float amount;
+
     private String comments;
 
 }
