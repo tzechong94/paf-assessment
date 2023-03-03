a1499c1f1522dbffccf6b30c62550624a4427819
tc94 <tze.chong1994@gmail.com>
2023-03-03 09:12:30 +0800

    spring initialised

---
+++ src/main/java/com/example/pafassessment/PafAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.pafassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafAssessmentApplication.class, args);
+	}
+
+}
